0ac1207a60de7201b185f0711be1e722f8f4d37c
Patrick McDavid <fusionjunky@gmail.com>
2023-01-29 15:33:30 -0700

    some trimming

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
 
-  
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <NavBar />
   <RouterView />
 </template>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  currentTab: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  
+  <header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><RouterLink class="nav-link px-2 text-secondary" to="/">Home</RouterLink></li>
+          <li><RouterLink class="nav-link px-2 text-white" to="/about">About</RouterLink></li>
+        </ul>
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2">Login</button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  .butt { color:brown }
+</style>
