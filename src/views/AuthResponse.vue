c91057a275048da411fdafa378ba351988fbc8f3
Patrick McDavid <fusionjunky@gmail.com>
2023-01-29 21:25:15 -0700

    made router go, thx perlpunk

--- src/views/AuthResponse.vue
+++ src/views/AuthResponse.vue
@@ -1,12 +1,12 @@
-<script>
+<script setup>
 import { useSessionStore } from '../stores/SessionStore'
+import router from '../router'
 const store = useSessionStore()
-
 let urlParams = new URLSearchParams(window.location.search);
 
 const jwt = urlParams.get('jwt')
 store.login(jwt)
-//router.push('/')
+router.push('/')
 
 </script>
 
