8c8a4b1733469da9923b271ce9ab4d9663533fdb
Patrick McDavid <fusionjunky@gmail.com>
2023-01-31 18:54:05 -0700

    Icons and buttons and sorting, oh my

---
+++ src/components/DiscordServerIcon.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    props: {
+        serverId: String,
+        icon: String,
+        name: String,
+        size: Number
+    },
+    computed: {
+        hasIcon() {
+            console.log()
+            return this.icon != null && this.icon != 'null'
+        },
+        serverAcronym() {
+            return this.name.split(' ').map(str => str[0]).join('')
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="serverIcon">
+        <img v-if="hasIcon" :src="'https://cdn.discordapp.com/icons/' + serverId + '/' + icon + '.webp?size=128'" />
+        <div v-else>
+            {{ serverAcronym }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.serverIcon {
+    border-radius: 100%;
+    background-color:gray;
+    display:inline-block;
+    height:96px;
+    width:96px;
+    text-align: center;
+    font-size: 50px;
+    margin-right: 10px;
+}
+.serverIcon img {
+    height:96px;
+    width:96;
+    border-radius: 100%;
+}
+</style>
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,5 +1,7 @@
 <script>
 import { useSessionStore } from '../stores/SessionStore'
+import AddToDiscordBtn from '../components/AddToDiscordBtn.vue'
+import DiscordServerIcon from '../components/DiscordServerIcon.vue'
 
 export default {
   data() {
@@ -9,11 +11,15 @@ export default {
       sessionStore: store
     }
   },
+  components: {
+    AddToDiscordBtn, DiscordServerIcon
+  },
   created() {
     if (this.sessionStore.isLoggedIn) {
       this.$api.getMyGames()
         .then(servers => {
           console.log("filled servers", servers.data)
+          servers.data.sort((a, b) => a.name.localeCompare(b.name))
           this.teams = servers.data
         }).catch(err => {
           console.log("Error with getMyGames", err)
@@ -33,13 +39,17 @@ export default {
         <h2>Log in to see your teams</h2>
       </div>
       <div v-else>
-        <h2>Here're your teams!</h2>
-        <ul>
-          <li v-for="team in teams">
-            {{ team.name }}
-          </li>
-        </ul>
-      </div>
+          <div v-for="team in teams" class="">
+            <h2><DiscordServerIcon :serverId="team.id" :icon="team.icon" :name="team.name" />{{ team.name }}
+              <AddToDiscordBtn :serverId="team.id" />
+            </h2>
+          </div>
+          <!-- https://cdn.discordapp.com/icons/880974616754012220/a_08d8e37712f1c7fa1383b162cf79cb3a.webp?size=96 -->
+      </div>880974616754012220
     </div>
   </main>
 </template>
+
+<style scoped>
+</style>
+
