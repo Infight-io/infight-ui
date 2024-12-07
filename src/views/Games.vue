a6b08b9ffaaaa943738bb3242a597c1618474fdc
Patrick McDavid <fusionjunky@gmail.com>
2024-12-06 23:35:43 -0700

    feat: add opt-in functionality for guilds and refactor guilds display logic in Games.vue

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -14,31 +14,45 @@ export default {
       botAppId: import.meta.env.VITE_DISCORD_BOT_APP_ID,
     }
   },
+  methods: {
+    sendOptIn(teamId, optIn) {
+      this.$api.postGuildOptIn(teamId, optIn)
+        .then(data => {
+          console.log("changed optIn", data)
+          this.refreshGuilds()
+        }).catch(err => {
+          console.log("Error with sendOptIn", err)
+        })
+    },
+    refreshGuilds() {
+      if (this.sessionStore.isLoggedIn) {
+        this.$api.getMyGames()
+          .then(servers => {
+            console.log("filled servers", servers.data)
+            servers.data.sort((a, b) => {
+
+              if (a.Guild.isConnected && !b.Guild.isConnected) {
+                return -1;
+              } else if (!a.Guild.isConnected && b.Guild.isConnected) {
+                return 1;
+              } else {
+                // If boolean values are equal, sort by the string property
+                return a.Guild.name.localeCompare(b.Guild.name);
+              }
+
+            })
+            this.teams = servers.data
+          }).catch(err => {
+            console.log("Error with getMyGames", err)
+          })
+      }
+    },
+  },
   components: {
     DiscordBtn, DiscordServerIcon
   },
   created() {
-    if (this.sessionStore.isLoggedIn) {
-      this.$api.getMyGames()
-        .then(servers => {
-          console.log("filled servers", servers.data)
-          servers.data.sort((a, b) => {
-
-            if (a.isConnected && !b.isConnected) {
-              return -1;
-            } else if (!a.isConnected && b.isConnected) {
-              return 1;
-            } else {
-              // If boolean values are equal, sort by the string property
-              return a.name.localeCompare(b.name);
-            }
-
-          })
-          this.teams = servers.data
-        }).catch(err => {
-          console.log("Error with getMyGames", err)
-        })
-    }
+    this.refreshGuilds()
   }
 }
 </script>
@@ -59,21 +73,34 @@ export default {
           </div>
           <div v-else>
             <div class="serverContainer">
-              <div v-for="team in teams" class="serverItem">
-                <DiscordServerIcon :serverId="team.id" :icon="team.icon" :name="team.name" />
-                <strong class="display-6">{{ team.name }}</strong>
-                <div class="discordBtnContainer" v-if="team.isConnected">
-                  <div v-if="team.currentGameId">
-                    <RouterLink :to="'/games/' + team.id + '/' + team.currentGameId">
-                      <button class="btn btn-primary">Play Now</button>
-                    </RouterLink>
+              <div v-for="pg in teams" class="serverItem container">
+                <div class="row">
+                  <div class="col-8">
+                    <DiscordServerIcon :serverId="pg.Guild.id" :icon="pg.Guild.icon" :name="pg.Guild.name" />
+                    <strong class="display-6">{{ pg.Guild.name }}</strong>
+                  </div>
+                  <div class="col-4 text-end">
+                      <span class="btn-group" v-if="pg.Guild.isConnected">
+                        <RouterLink class="btn btn-primary" v-if="pg.Guild.currentGameId"
+                          :to="'/games/' + pg.Guild.id + '/' + pg.Guild.currentGameId">Play Now</RouterLink>
+
+                        <button class="btn btn-success" v-if="!pg.isOptedInToPlay && pg.Guild.isConnected"
+                          @click="sendOptIn(pg.Guild.id, true)">Opt in</button>
+
+                        <button class="btn btn-warning" v-if="pg.isOptedInToPlay && pg.Guild.isConnected"
+                          @click="sendOptIn(pg.Guild.id, false)">Opt out</button>
+                      </span>
+
+                
+                    <span class="discordBtnContainer" v-if="!pg.Guild.isConnected">
+                      <DiscordBtn
+                        :url="`https://discord.com/api/oauth2/authorize?client_id=${botAppId}&permissions=275951650832&scope=bot%20applications.commands`"
+                        target="_blank">Add</DiscordBtn>
+                    </span>
                   </div>
                 </div>
-                <div class="discordBtnContainer" v-if="!team.isConnected">
-                  <DiscordBtn
-                    :url="`https://discord.com/api/oauth2/authorize?client_id=${botAppId}&permissions=275951650832&scope=bot%20applications.commands`"
-                    target="_blank">Add to Discord</DiscordBtn>
-                </div>
+                
+
               </div>
             </div>
           </div>
@@ -87,7 +114,9 @@ export default {
             edge out your friends.
           </p>
 
-          <p><RouterLink to="/FAQ">Check out the rules</RouterLink> and invite your friends!</p>
+          <p>
+            <RouterLink to="/FAQ">Check out the rules</RouterLink> and invite your friends!
+          </p>
 
           <img src="/img/homepageSplash.png" class="img-fluid" alt="an Infight game board">
         </div>
@@ -114,7 +143,5 @@ export default {
 }
 
 .discordBtnContainer {
-  float: right !important;
-  margin-top: 18px;
 }
 </style>
